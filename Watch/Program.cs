16cd31016194a30b6f265f055ac0de086ce014dc
xql <985045893@qq.com>
2017-03-01 22:49:13 +0800

    3/1

---
+++ Watch/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Watch
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            deleteLog();
+            new Program().watching();
+        }
+        private void watching()
+        {
+            log myLog = new log("D://log", "/Watch.log");
+            int startCounts = 0;
+            Process watch = new Process();
+            try
+            {
+                watch = Process.Start("AVR_3.exe");
+                startCounts++;
+                myLog.WriteLog("start counts", startCounts.ToString());              
+            }
+            catch(Exception e){
+                myLog.WriteLog("start failed", e.ToString());
+            }
+            while (true)
+            {
+                if (watch.HasExited)
+                {
+                    myLog.WriteLog("process stoped!", "null");
+                    Thread.Sleep(2000);
+                    watching();
+                }                           
+            }
+        }
+
+        static void deleteLog()
+        {
+            String[] filename =
+            {
+                @"D:\\log\\event.log", @"D:\\log\\main.log", @"D:\\log\\tcp.log", @"D:\\log\\video.log",
+                @"D:\\log\\Watch.log", @"D:\\log\\gpio.log"
+            };
+            for (int i = 0; i < filename.Length; i++)
+            {
+                if (File.Exists(filename[i]))
+                {
+                    //如果存在则删除
+                    File.Delete(filename[i]);
+                } 
+            }
+        }
+    }
+}
---
+++ Watch/log.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace Watch
+{
+    class log
+    {
+        private string LogPath; //= "D://log";  
+        private string LogName; // = "/Watch.log";  
+
+        public log(string path, string name)
+        {
+            LogPath = path;
+            LogName = "/" + name;
+        }
+
+        public void WriteLog(string log1, string log2)
+        {
+            try
+            {
+                DirectoryInfo d = Directory.CreateDirectory(LogPath);
+                FileStream fs = new FileStream(LogPath + LogName, System.IO.FileMode.Append);
+                StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.Default);
+                sw.WriteLine(DateTime.Now.ToString() + "\t" + log1 + "\t" + log2);
+                sw.Close();
+                fs.Close();
+            }
+            catch
+            {
+                //    // Nothing to do  
+            }
+        }
+    }
+}
